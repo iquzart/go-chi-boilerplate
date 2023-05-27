f117a366d5db6fa50439dda484c81af9dba4a8d1
Muhammed Iqbal <iquzart@hotmail.com>
2023-05-27 10:40:14 +0530

    routes

---
+++ routes/apiRoutes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	controllers "go-chi-boilerplate/controllers/api"
+
+	"github.com/go-chi/chi"
+)
+
+// addAPIRoutes adds the routes for the API controller to the specified router group.
+func addAPIRoutes(rg chi.Router) {
+	// Create a new group of routes for the API controller under the specified router group.
+	api := chi.NewRouter()
+
+	// Add a GET route for the APIVersion method of the API controller.
+	api.Get("/version", controllers.APIVersion)
+
+	// Mount the API routes under the specified router group.
+	rg.Mount("/api", api)
+}
