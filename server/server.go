8ab33f328536c3395053b39a0dd13e841a22db8a
Muhammed Iqbal <iquzart@hotmail.com>
2023-05-27 20:39:24 +0530

    added Service name and updated the function names

--- server/server.go
+++ server/server.go
@@ -16,6 +16,7 @@ import (
 type serverConfigs struct {
 	port             string // The port number on which to start the server.
 	gracefulShutdown bool   // Whether to use graceful shutdown when stopping the server.
+	serviceName      string // The service name to configure on logs and metrics
 }
 
 // Run starts the HTTP server.
@@ -24,7 +25,7 @@ func Run() {
 	serverConfigs := getConfigs()
 
 	// Initialize the router with the application's routes.
-	router := routes.InitRouter()
+	router := routes.SetupRouter(serverConfigs.serviceName)
 
 	// Create an HTTP server with the specified address and router.
 	server := &http.Server{
@@ -34,9 +35,9 @@ func Run() {
 
 	// Start the server with or without graceful shutdown.
 	if serverConfigs.gracefulShutdown {
-		startWithGracefulShutdown(server)
+		startWithGracefulShutdown(serverConfigs.serviceName, server)
 	} else {
-		start(server)
+		start(serverConfigs.serviceName, server)
 	}
 }
 
@@ -46,6 +47,13 @@ func getConfigs() *serverConfigs {
 	if port == "" {
 		port = "8080"
 	}
+
+	// Get the serviceName from the environment variable or use a default value
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		serviceName = "go-chi-boilerplate"
+	}
+
 	gracefulShutdown, err := strconv.ParseBool(os.Getenv("ENABLE_GRACEFUL_SHUTDOWN"))
 	if err != nil {
 		gracefulShutdown = true
@@ -53,20 +61,21 @@ func getConfigs() *serverConfigs {
 	return &serverConfigs{
 		port:             fmt.Sprintf(":%s", port),
 		gracefulShutdown: gracefulShutdown,
+		serviceName:      serviceName,
 	}
 }
 
 // start starts the HTTP server without graceful shutdown.
-func start(server *http.Server) {
-	log.Printf("Server started on port %s", server.Addr)
+func start(serviceName string, server *http.Server) {
+	log.Printf("Started %s service on port %s", serviceName, server.Addr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Error starting server: %s\n", err)
 	}
 }
 
 // startWithGracefulShutdown starts the HTTP server with graceful shutdown.
-func startWithGracefulShutdown(server *http.Server) {
-	log.Printf("Started the server on port %s with graceful shutdown ", server.Addr)
+func startWithGracefulShutdown(serviceName string, server *http.Server) {
+	log.Printf("Started %s service on port %s with graceful shutdown ", serviceName, server.Addr)
 
 	// Create a channel to signal when all idle connections are closed.
 	idleConnsClosed := make(chan struct{})
